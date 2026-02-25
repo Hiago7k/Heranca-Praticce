c802e511d999222281a3aef4f6eb36bee969ffcf
Hiago 7k <hiago7k@gmail.com>
2026-02-24 21:52:16 -0300

    Adicionar arquivos de projeto.

---
+++ DificuldadeDoJogo.cs
@@ -0,0 +1,10 @@
+﻿namespace Heranca_Praticce;
+
+internal class DificuldadeDoJogo
+{
+    public DificuldadeDoJogo(string dificuldadeDoModol)
+    {
+        DificuldadeDoModo = dificuldadeDoModol;
+    }
+    public string DificuldadeDoModo { get; set; }
+}
---
+++ Jogador.cs
@@ -0,0 +1,16 @@
+﻿namespace Heranca_Praticce;
+
+internal class Jogador
+{
+    public Jogador(string nomeDoJogador, int idadeDoJogador, DificuldadeDoJogo dificuldade)
+    {
+        NomeDoJogador = nomeDoJogador;
+        IdadeDoJogador = idadeDoJogador;
+        Dificuldade = dificuldade;
+    }
+
+    public string NomeDoJogador { get;  }
+    public int IdadeDoJogador { get;  }
+    public DificuldadeDoJogo Dificuldade { get; set; }
+
+}
